87107116deff1c59ec826f994e157d4131950154
Zbigniew Dynowski <dynowski@basement.pl>
2022-06-14 11:50:32 +0200

    delete account dialog

--- app/src/main/java/pl/dynovski/quizerr/activities/AccountDetailsActivity.kt
+++ app/src/main/java/pl/dynovski/quizerr/activities/AccountDetailsActivity.kt
@@ -1,5 +1,7 @@
 package pl.dynovski.quizerr.activities
 
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
@@ -65,53 +67,65 @@ class AccountDetailsActivity: AppCompatActivity() {
             }
 
         deleteAccountButton.setOnClickListener {
-            val userId = LoggedUser.get().userId
-            auth.currentUser!!.delete()
-                .addOnSuccessListener {
-                    database.collection("Users")
-                        .document(userId)
-                        .delete()
-                        .addOnSuccessListener {
-                            Toast.makeText(
-                                this,
-                                R.string.action_delete_success,
-                                Toast.LENGTH_SHORT
-                            ).show()
-                            database.collection("Courses")
-                                .whereArrayContains("enrolledUsersIds", userId)
-                                .get()
-                                .addOnSuccessListener {
-                                    for (document in it.documents) {
-                                        database.collection("Courses")
-                                            .document(document.id)
-                                            .update("enrolledUsersIds", FieldValue.arrayRemove(userId))
-                                            .addOnSuccessListener {
-                                                Log.d(TAG, "Deleted $userId from enrolled users of ${document.id}")
-                                            }
-                                    }
-                                }
-                            LoggedUser.logout()
-                            val intent = Intent(this, SignInActivity::class.java)
-                            intent.addFlags(
-                                Intent.FLAG_ACTIVITY_NEW_TASK
-                                        or Intent.FLAG_ACTIVITY_CLEAR_TASK
-                                        or Intent.FLAG_ACTIVITY_CLEAR_TOP
-                            )
-                            startActivity(intent)
-                            finish()
-                        }
-                        .addOnFailureListener {
-                            Log.d(TAG, "Could not delete user from firestore\n$it")
-                        }
-                }
-                .addOnFailureListener {
-                    Log.d(TAG, "Could not delete user from auth\n$it")
-                    Toast.makeText(
-                        this,
-                        R.string.action_delete_fail,
-                        Toast.LENGTH_LONG
-                    ).show()
-                }
+            val builder = AlertDialog.Builder(this)
+            builder.setMessage(R.string.question_delete_account)
+            builder.setPositiveButton(R.string.action_yes) { dialog: DialogInterface, _: Int ->
+                dialog.dismiss()
+                deleteAccount(LoggedUser.get().userId)
+            }
+            builder.setNegativeButton(R.string.action_no) { dialog: DialogInterface, _: Int ->
+                dialog.dismiss()
+            }
+            builder.create().show()
         }
+    }
+
+    private fun deleteAccount(userId: String) {
+        auth.currentUser!!.delete()
+            .addOnSuccessListener {
+                database.collection("Users")
+                    .document(userId)
+                    .delete()
+                    .addOnSuccessListener {
+                        Toast.makeText(
+                            this,
+                            R.string.action_delete_success,
+                            Toast.LENGTH_SHORT
+                        ).show()
+                        database.collection("Courses")
+                            .whereArrayContains("enrolledUsersIds", userId)
+                            .get()
+                            .addOnSuccessListener {
+                                for (document in it.documents) {
+                                    database.collection("Courses")
+                                        .document(document.id)
+                                        .update("enrolledUsersIds", FieldValue.arrayRemove(userId))
+                                        .addOnSuccessListener {
+                                            Log.d(TAG, "Deleted $userId from enrolled users of ${document.id}")
+                                        }
+                                }
+                            }
+                        LoggedUser.logout()
+                        val intent = Intent(this, SignInActivity::class.java)
+                        intent.addFlags(
+                            Intent.FLAG_ACTIVITY_NEW_TASK
+                                    or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                                    or Intent.FLAG_ACTIVITY_CLEAR_TOP
+                        )
+                        startActivity(intent)
+                        finish()
+                    }
+                    .addOnFailureListener {
+                        Log.d(TAG, "Could not delete user from firestore\n$it")
+                    }
+            }
+            .addOnFailureListener {
+                Log.d(TAG, "Could not delete user from auth\n$it")
+                Toast.makeText(
+                    this,
+                    R.string.action_delete_fail,
+                    Toast.LENGTH_LONG
+                ).show()
+            }
     }
 }
